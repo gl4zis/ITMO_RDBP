56e371d16161bcc3b1b1e26eca1f6d1263dd6f3e
gl4zis <maker64@inbox.ru>
2026-01-05 23:16:36 +0300

    move payment api on openapi

--- server/src/main/java/ru/itmo/is/controller/PaymentController.java
+++ server/src/main/java/ru/itmo/is/controller/PaymentController.java
@@ -1,34 +1,36 @@
 package ru.itmo.is.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.itmo.is.dto.request.PaymentRequest;
-import ru.itmo.is.dto.response.PaymentResponse;
+import ru.itmo.is.api.PaymentApi;
+import ru.itmo.is.dto.PaymentRequest;
+import ru.itmo.is.dto.PaymentResponse;
 import ru.itmo.is.entity.user.User;
 import ru.itmo.is.security.RolesAllowed;
 import ru.itmo.is.service.PaymentService;
 
 @RestController
-@RequestMapping("/payment")
 @RequiredArgsConstructor
-public class PaymentController {
+public class PaymentController implements PaymentApi {
     private final PaymentService paymentService;
 
+    @Override
     @RolesAllowed(User.Role.RESIDENT)
-    @GetMapping("/info/self")
-    public PaymentResponse getSelfPaymentInfo() {
-        return paymentService.getSelfPaymentInfo();
+    public ResponseEntity<PaymentResponse> getSelfPaymentInfo() {
+        return ResponseEntity.ok(paymentService.getSelfPaymentInfo());
     }
 
+    @Override
     @RolesAllowed(User.Role.MANAGER)
-    @GetMapping("/info")
-    public PaymentResponse getPaymentInfo(@RequestParam("login") String login) {
-        return paymentService.getPaymentInfo(login);
+    public ResponseEntity<PaymentResponse> getPaymentInfo(String login) {
+        return ResponseEntity.ok(paymentService.getPaymentInfo(login));
     }
 
+    @Override
     @RolesAllowed(User.Role.RESIDENT)
-    @PostMapping("/pay")
-    public void pay(@RequestBody PaymentRequest req) {
+    public ResponseEntity<Void> pay(PaymentRequest req) {
         paymentService.currentUserPay(req);
+        return ResponseEntity.ok().build();
     }
 }
